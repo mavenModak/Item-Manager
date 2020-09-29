c25911579d295edf0fc761f67a3d48a1f87e76e9
Hasan Banna <65730033+mavenModak@users.noreply.github.com>
2020-09-30 01:17:00 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCKf3Z5EP3T7ILxbiQ29BwWcB1jwYom-T8",
-    authDomain: "item-manager-8db1b.firebaseapp.com",
-    databaseURL: "https://item-manager-8db1b.firebaseio.com",
-    projectId: "item-manager-8db1b",
-    storageBucket: "item-manager-8db1b.appspot.com",
-    messagingSenderId: "597457451014",
-    appId: "1:597457451014:web:00ec07a7a6acc5aecc2b87"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
   }
 };
 
